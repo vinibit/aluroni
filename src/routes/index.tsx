42d2cdcbda5bd2e345a6bee40e73c4c6d1f07455
Vinicius Martins <vinibitmartins@gmail.com>
2025-01-15 19:11:49 -0300

    feat: Adiciona cabeçalho estilizado e reorganiza a estrutura do Cardápio

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import styles from './Routes.module.scss'
 
 import Inicio from 'pages/Inicio'
 import Cardapio from 'pages/Cardapio'
@@ -6,13 +7,20 @@ import Menu from 'components/Menu'
 
 export default function AppRouter() {
   return (
-    <Router>
-      <Menu />
-      <Routes>
+    <main>
+      <Router>
+        <Menu />
+        <header className={styles.header}>
+          <div className={styles.header__text}>
+            A casa do código e da massa
+          </div>
+        </header>
+        <Routes>
           <Route path='/' element={<Inicio />} />
           <Route path='/cardapio' element={<Cardapio />} />
           <Route path='/sobre' element={null} />
-      </Routes>
-    </Router>
+        </Routes>
+      </Router>
+    </main>
   )
 }
